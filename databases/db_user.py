71d4a5bc3c9a31c3812b8211ce8b5d5b2589eac2
VukoticLuka <lukavukotic811@gmail.com>
2024-05-16 17:20:34 +0200

    add create user function

--- databases/db_user.py
+++ databases/db_user.py
@@ -1,4 +1,18 @@
+from sqlmodel import Session
+
+from databases.database import engine
+from databases.models import DbUser
+from schemas import UserBase
 
 
-async def create_user():
-    pass
+async def create_user(request: UserBase):
+    with Session(engine) as session:
+        user = DbUser(
+            username=request.username,
+            email=request.email,
+            password=request.password
+        )
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
