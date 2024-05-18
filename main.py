9c832fb66800fb76b4bcc138248f06850ac88f4a
VukoticLuka <lukavukotic811@gmail.com>
2024-05-18 07:18:03 +0200

    connect user router to main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from sqlmodel import SQLModel
 
 from databases.database import engine
-
+from routers import user
 
 app = FastAPI()
+
+app.include_router(user.router)
 
 SQLModel.metadata.create_all(engine)
---
+++ routers/user.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from databases import db_user
+from schemas import UserBase, UserDisplay
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+
+@router.post('/new', response_model=UserDisplay)
+async def create_user(request: UserBase):
+    return db_user.create_user(request)
