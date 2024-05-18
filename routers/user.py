30717f6f8f7c997311965411344aab281e925a0c
VukoticLuka <lukavukotic811@gmail.com>
2024-05-18 07:36:31 +0200

    Added endpoint for get_user functionality

--- routers/user.py
+++ routers/user.py
@@ -11,3 +11,8 @@ router = APIRouter(
 @router.post('/new', response_model=UserDisplay)
 async def create_user(request: UserBase):
     return await db_user.create_user(request)
+
+
+@router.get('/{id}', response_model=UserDisplay)
+async def get_user(id: int):
+    return await db_user.get_user(id)
